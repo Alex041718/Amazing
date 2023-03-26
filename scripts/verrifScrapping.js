c4dce2a676da6568c707e43e3e334a78482b5781
Alexandre <Alexandre.alm18@gmail.com>
2023-03-27 01:09:20 +0200

    euuh mon propre user agent

--- scripts/verrifScrapping.js
+++ scripts/verrifScrapping.js
@@ -8,7 +8,7 @@ async function getPrice(url) {
       const options = {
         url: url,
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'
         }
       };
       
