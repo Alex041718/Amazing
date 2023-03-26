3a948804084a7f5a85fc4ba2e5a5ef2cdd50ecfe
Alexandre <Alexandre.alm18@gmail.com>
2023-03-27 01:35:51 +0200

    regeg

--- scripts/verrifScrapping.js
+++ scripts/verrifScrapping.js
@@ -1,10 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const fs = require('fs');
 
 const url = 'https://www.amazon.fr/15ITL6-I5-1135G7-512GB-15-6IN-NOOD/dp/B091CVM6W2?ref_=Oct_DLandingS_D_5a6dc67a_61';
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
-const ca = fs.readFileSync('../data/certificatAmazon.pem');
 
 async function getPrice(url, userAgent) {
     try {
@@ -13,8 +11,7 @@ async function getPrice(url, userAgent) {
         headers: {
           'User-Agent': userAgent
         },
-        strictSSL: true,
-        ca: ca
+        strictSSL: true
       };
       
       const response = await new Promise((resolve, reject) => {
