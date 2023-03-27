649307181715e4cbdd03c60d40a6dd1c668d1d90
Alexandre <Alexandre.alm18@gmail.com>
2023-03-27 15:27:49 +0200

    ça marche parfaitement avec puppeteer sur mac

--- scripts/getAmazonProduct.js
+++ scripts/getAmazonProduct.js
@@ -1,41 +1,29 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const getTimeStamp = require('./getTimeStamp');
 
-
-//const url = 'https://www.amazon.fr/15ITL6-I5-1135G7-512GB-15-6IN-NOOD/dp/B091CVM6W2?ref_=Oct_DLandingS_D_5a6dc67a_61';
+//const url = 'https://www.amazon.fr/Apple-MX532ZMA-Nouveau-AirTag/dp/B0935DN1BN/ref=sr_1_1_sspa?keywords=airtags&qid=1678472001&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1';
 
 async function getPrice(url) {
-  try {
-    
-    const response = await new Promise((resolve, reject) => {
-      request(url, (error, response, html) => {
-        if (error) reject(error);
-        else resolve(response);
-      });
-    });
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    if (response.statusCode !== 200) {
-      throw new Error('Failed to load page');
-    }
+  const name = await page.$eval('#centerCol #title_feature_div #titleSection #title #productTitle', element => element.textContent);
+  const price = await page.$eval('span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay .a-offscreen', element => element.textContent);
+  
+  const res = {};
+  res["name"] = name;
+  res["price"] = parseFloat(price.split('€')[0]);
+  res["timestamp"] = getTimeStamp();
+  res["url"] = url;
 
-    const $ = cheerio.load(response.body);
 
-    const price = $('span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay .a-offscreen').text();
-    const titleProduct = $('#centerCol #title_feature_div #titleSection #title #productTitle').text();
-    const res = {};
-
-    res["name"] = titleProduct.split(',')[0];
-    res["price"] = parseFloat(price.split('€')[0]);
-    res["timestamp"] = getTimeStamp();
-    res["url"] = url;
-
-    return res;
-  } catch (error) {
-    console.error(error);
-    return 'error';
-  }
-}
+  await browser.close();
+  //console.log(res);
+  return res
+  //await page.screenshot({path: 'example.png'});
+  
+};
 
 //getPrice(url).then(res => console.log(res));
 
