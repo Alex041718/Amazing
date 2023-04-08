39d1a96178a9d958d0cce7e3682e7e6f3cd6b85b
Alexandre <Alexandre.alm18@gmail.com>
2023-04-08 14:57:03 +0200

    Update addProduct.php

--- site/addProduct.php
+++ site/addProduct.php
@@ -1,74 +1,83 @@
-<?php
-// Définir la constante ASIN avec une valeur
+<html>
 
-$asin = $_GET['asin'];
+<head></head>
 
+<body>
 
-echo("Ajout en cours ...");
-// -------------- AJOUT DU PRODUIT DANS LA BASE DE DONNÉES
-// Construire l'URL de la requête en utilisant la valeur de la constante ASIN
-$url = "http://51.38.35.91:3000/products/addProduct?ASIN={$asin}";
+  <?php
+  // Définir la constante ASIN avec une valeur
 
-$curl = curl_init();
+  $asin = $_GET['asin'];
 
 
-// Requête GET de l'API
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-));
+  echo ("Ajout en cours ...");
+  // -------------- AJOUT DU PRODUIT DANS LA BASE DE DONNÉES
+  // Construire l'URL de la requête en utilisant la valeur de la constante ASIN
+  $url = "http://51.38.35.91:3000/products/addProduct?ASIN={$asin}";
 
-$response = curl_exec($curl);
-
-curl_close($curl);
-
-sleep(2);
+  $curl = curl_init();
 
 
-// -------------- SCRAPPING POUR L'UNIQUE PRODUIT
+  // Requête GET de l'API
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
 
-$url = "http://51.38.35.91:3000/products/pushOneProduct?ASIN={$asin}";
+  $response = curl_exec($curl);
 
-$curl = curl_init();
+  curl_close($curl);
 
-
-// Requête GET de l'API
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-
-sleep(1);
+  sleep(2);
 
 
+  // -------------- SCRAPPING POUR L'UNIQUE PRODUIT
 
-// Redirection vers la page d'accueil avec un paramètre successAdd
-//echo "Location: https://amazing.alexandre-le-marec.fr/index.php?asin={$asin}&successAdd=true"
-//header("Location: https://amazing.alexandre-le-marec.fr/index.php?asin={$asin}&successAdd=true");
-exit();
+  $url = "http://51.38.35.91:3000/products/pushOneProduct?ASIN={$asin}";
 
-echo("
+  $curl = curl_init();
+
+
+  // Requête GET de l'API
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+
+  sleep(1);
+
+
+
+  // Redirection vers la page d'accueil avec un paramètre successAdd
+  //echo "Location: https://amazing.alexandre-le-marec.fr/index.php?asin={$asin}&successAdd=true"
+  //header("Location: https://amazing.alexandre-le-marec.fr/index.php?asin={$asin}&successAdd=true");
+  exit();
+
+  echo ("
 <script>
   
   window.location.assign('https://amazing.alexandre-le-marec.fr/index.php?asin=<?php echo $asin ?>&successAdd=true');
 </script>
 ");
 
-?>
+  ?>
 
+</body>
+
+</html>
