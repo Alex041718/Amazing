80371ef0f931b34f793831cf651c5e53710cfa07
Alexandre <Alexandre.alm18@gmail.com>
2023-04-20 14:50:10 +0200

    Update getDataSearchOnAmazon.js

--- scripts/getDataSearchOnAmazon.js
+++ scripts/getDataSearchOnAmazon.js
@@ -23,7 +23,7 @@ async function getListSearch(search,nbProduits) {
                     var root = parse(response.data);
                     var res = [];
 
-                    for (let index = 7; index < nbProduits+8; index++) {
+                    for (let index = 7; index < nbProduits+7; index++) {
                         asin = root.querySelector(`#search > div.s-desktop-width-max.s-desktop-content.s-wide-grid-style-t3.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(${index})`).getAttribute("data-asin");
 
                         if (asin != '') {
